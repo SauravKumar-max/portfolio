77c6bf782e7173c19a7dc145dc279e71a4db8daa
Saurav Kumar <maxsaurav237@gmail.com>
2023-06-22 20:12:12 +0530

    style: about chages

--- src/components/Profile/About.tsx
+++ src/components/Profile/About.tsx
@@ -11,7 +11,7 @@ export function About() {
         <div>
           <h1 className="text-4xl sm:text-5xl font-bold">Saurav Kumar</h1>
           <h2 className="flex items-start gap-1 text-xs sm:text-base font-mono">
-            <span>Frontend Developer </span>
+            <span className="whitespace-nowrap">Frontend Developer </span>
             <span>/</span>
             <span>Full Stack Developer</span>
           </h2>
